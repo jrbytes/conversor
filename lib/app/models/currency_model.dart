b592287a21618cf6a45e1e5ba3afb0f8c32511c7
Jr Bytes <itamazor@gmail.com>
2022-11-05 15:10:46 -0300

    updated model to use enum, added tests to convertation

--- lib/app/models/currency_model.dart
+++ lib/app/models/currency_model.dart
@@ -1,5 +1,12 @@
+enum CurrencyType {
+  REAL,
+  DOLAR,
+  EURO,
+  BITCOIN,
+}
+
 class CurrencyModel {
-  final String name;
+  final CurrencyType name;
   final double real;
   final double dolar;
   final double euro;
@@ -16,17 +23,32 @@ class CurrencyModel {
   static List<CurrencyModel> getCurrencies() {
     return <CurrencyModel>[
       CurrencyModel(
-          name: 'Real', real: 1.0, dolar: 0.18, euro: 0.15, bitcoin: 0.000016),
+        name: CurrencyType.REAL,
+        real: 1.0,
+        dolar: 0.18,
+        euro: 0.15,
+        bitcoin: 0.000016,
+      ),
       CurrencyModel(
-          name: 'Dolar', real: 5.63, dolar: 1.0, euro: 0.85, bitcoin: 0.000088),
+        name: CurrencyType.DOLAR,
+        real: 5.65,
+        dolar: 1.0,
+        euro: 0.85,
+        bitcoin: 0.000088,
+      ),
       CurrencyModel(
-          name: 'Euro', real: 6.62, dolar: 1.17, euro: 1.0, bitcoin: 0.00010),
+        name: CurrencyType.EURO,
+        real: 6.62,
+        dolar: 1.17,
+        euro: 1.0,
+        bitcoin: 0.00010,
+      ),
       CurrencyModel(
-          name: 'Bitcoin',
+          name: CurrencyType.BITCOIN,
           real: 64116.51,
           dolar: 11351.30,
-          euro: 9689.34,
-          bitcoin: 1.0),
+          euro: 9689.54,
+          bitcoin: 1),
     ];
   }
 }
---
+++ test/app/controllers/home_controller_test.dart
@@ -0,0 +1,29 @@
+import 'package:conversor_curso/app/controllers/home_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  final TextEditingController toText = TextEditingController();
+  final TextEditingController fromText = TextEditingController();
+  final homeController = HomeController(toText: toText, fromText: fromText);
+
+  test('deve converter de real para dólar com vírgula', () {
+    toText.text = '2,0';
+    homeController.convert();
+    expect(fromText.text, '0.36');
+  });
+
+  test('deve converter de real para dólar', () {
+    toText.text = '2.0';
+    homeController.convert();
+    expect(fromText.text, '0.36');
+  });
+
+  test('deve converter de dólar para real', () {
+    toText.text = '1.0';
+    homeController.toCurrency = homeController.currencies[1];
+    homeController.fromCurrency = homeController.currencies[0];
+    homeController.convert();
+    expect(fromText.text, '5.65');
+  });
+}
